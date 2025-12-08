9ff2a0bb0171b3df2c013265cdb29e7a14601f42
Darya Frank <darya.frank@gmail.com>
2025-12-08 12:56:01 +0000

    removed old files

--- tapas_similarity_exposure_precomputed_transp.m
+++
@@ -1,9 +0,0 @@
-function [pvec, pstruct] = tapas_similarity_exposure_precomputed_transp(r, ptrans)
-pvec = NaN(1,length(ptrans));
-pvec(1) = exp(ptrans(1));  % ze
-pvec(2) = ptrans(2);       % beta_exposure
-
-pstruct = struct();
-pstruct.ze = pvec(1);
-pstruct.beta_exposure = pvec(2);
-return;
